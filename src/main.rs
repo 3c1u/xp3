258d5acb7bebd9fdb2f53b8c2f6f95c9db1795d2
9646516 <zyq855@gmail.com>
2019-07-18 03:37:49 +0800

    add clap

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 #![allow(dead_code)]
 
 use std::fs;
+use clap::Arg;
+use clap::App;
 
 pub mod Header;
 pub mod Segment;
@@ -12,8 +14,27 @@ pub mod XP3Info;
 pub mod extent;
 pub mod utils;
 fn main() {
-    let path = "./test/1.xp3";
+    let matches = App::new("XP3Parser")
+        .version("1.0.0")
+        .author("9646516 <zyq855@gmail.com>")
+        .arg(Arg::with_name("Sourse")
+            .short("s")
+            .value_name("Sourse")
+            .help("Path of XP3 File")
+            .takes_value(true)
+            .required(true)
+        )
+        .arg(Arg::with_name("Desk")
+            .short("d")
+            .value_name("Desk")
+            .help("Path of Output Directory")
+            .takes_value(true)
+            .required(true)
+        )
+        .get_matches();
+    let path = matches.value_of("Sourse").expect("Need Sourse");
+    let desk = matches.value_of("Desk").expect("Need Desk");
     let data = fs::read(path).expect("path not valid");
     let res = Solve::unpack(&data).expect("Exact Failed");
-    res.extract("./test/123");
+    res.extract(desk);
 }
